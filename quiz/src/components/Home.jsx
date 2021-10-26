df4add0ecf10b5ec5919255f151484cb3640f0e8
Rajesh kumar <rajeshkumar.d190@gmail.com>
2021-10-26 20:11:07 +0530

    mern-74

---
+++ quiz/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div id='container'>
+                <h1>Quiz App</h1>
+                <button>Play</button>
+            </div>
+        )
+    }
+}
+
